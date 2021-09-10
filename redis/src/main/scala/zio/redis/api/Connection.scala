1490b1ec0e4b357e41c522e65a179f9c75319dfb
mvelimir <velimirmilinkovic1@gmail.com>
2021-09-10 09:29:15 +0200

    Debug

--- redis/src/main/scala/zio/redis/api/Connection.scala
+++ redis/src/main/scala/zio/redis/api/Connection.scala
@@ -116,8 +116,8 @@ trait Connection {
    */
   final def clientList(
     ids: Long*
-  )(clientType: Option[ClientType] = None): ZIO[RedisExecutor, RedisError, Chunk[ClientInfo]] = {
-    val command = RedisCommand(ClientList, Tuple2(OptionalInput(ClientTypeInput), IdsInput), ClientInfoOutput)
+  )(clientType: Option[ClientType] = None): ZIO[RedisExecutor, RedisError, Unit] = {
+    val command = RedisCommand(ClientList, Tuple2(OptionalInput(ClientTypeInput), IdsInput), UnitOutput)
 
     command.run((clientType, ids.toList))
   }
